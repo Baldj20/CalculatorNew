b58a8f0d0d1e131f0f5d197bfb4d78cf6b4ec3dd
Baldj <mihal275@mail.ru>
2024-07-05 14:11:07 +0300

    Added classes Parsing, Validation, WarningMessage

---
+++ WarningMessage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculatorNew
+{
+    internal static class WarningMessage
+    {
+        public static void ThrowErrorMessage()
+        {
+            MessageBox.Show("Incorrect input!", "Warning!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        }
+    }
+}
